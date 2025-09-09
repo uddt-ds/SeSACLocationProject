99037d3f68f8057147480c2e12339f6e6590d82b
우다당탕 개발자 <uddt_ds@kakao.com>
2025-09-09 17:56:45 +0900

    Initial Commit

---
+++ SeSACLocationProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSACLocationProject
+//
+//  Created by Lee on 9/9/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
